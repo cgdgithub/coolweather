bcba3cccb405c1f6954333d0785bb9baffd1b820
caogeng <2496977765@qq.com>
2016-03-28 14:11:53 +0800

    新增数据库帮组类，以及各表对应的实体类。

---
+++ src/com/example/m_coolweather/activity/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.m_coolweather.activity;
+
+import com.example.m_coolweather.R;
+import com.example.m_coolweather.R.layout;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+	}
+
+}
